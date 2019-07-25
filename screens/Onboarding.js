38364d06457c420cade50e240b521ea77b598678
Dercont <armando.almengor@utp.ac.pa>
2019-07-24 19:00:59 -0500

    Fin de Pruebas

--- screens/Onboarding.js
+++ screens/Onboarding.js
@@ -29,7 +29,7 @@ export default class Onboarding extends React.Component {
                 shadowless
                 style={styles.button}
                 color={materialTheme.COLORS.INFO}
-                onPress={() => navigation.navigate('Login')}>
+                onPress={() => navigation.navigate('Home')}>
                 Empezar
               </Button>
             </Block>
