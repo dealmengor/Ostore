c317ca543425af40c1ac45cbe9c684bb68f06fc0
Alexander Almengor <alexanderalmengor1@gmail.com>
2019-07-25 22:02:25 -0500

    Actualización

--- screens/ProductsForm.js
+++ screens/ProductsForm.js
@@ -1,128 +1,100 @@
 import React from 'react';
-
-import { ImageBackground, StyleSheet, StatusBar, Dimensions, Image, icon,AsyncStorage,Alert} from 'react-native';
-import { Block, Button, Text, theme,Input } from 'galio-framework'
-import { Select, Icon, Header, Product, Switch } from '../components/';
-
+import { ImageBackground, Image, StyleSheet, StatusBar, Dimensions, Platform } from 'react-native';
+import { Block, Button, Text, theme } from 'galio-framework';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const { height, width } = Dimensions.get('screen');
+import { Images, materialTheme } from '../constants/';
+import { HeaderHeight } from "../constants/utils";
 
-import materialTheme from '../constants/Theme';
-import Images from '../constants/Images';
-
-export default class Login extends React.Component {
-  state = {
-    username: '',
-    password: ''
-  };
-
-  onChangeText = (key, val) => {
-    this.setState({ [key]: val});
-  }
-
+export default class Pro extends React.Component {
   render() {
     const { navigation } = this.props;
 
     return (
       <Block flex style={styles.container}>
         <StatusBar barStyle="light-content" />
-        <Block flex center>
+        <Block flex>
           <ImageBackground
-            source={{  uri: Images.ImgLogin }}
-            style={{ height: height, width: width, zIndex: 1 }}
-          />
-        </Block>
-        <Block flex space="between" style={styles.padded}>
-          <Block flex space="around" style={{ zIndex: 2 }}>
-            <Block center >
-              <Input
-                right
-                placeholder="Usuario"
-                onChangeText={val => this.onChangeText('Nombre', val)}
-                placeholderTextColor={materialTheme.COLORS.DEFAULT}
-                style={{ borderRadius: 3, borderColor: materialTheme.COLORS.INPUT }}
-                iconContent={<Icon size={16} color={theme.COLORS.ICON} name="user" family="font-awesome" />}
-              />
-              <Input
-                right
-                placeholder="Contraseña"
-                onChangeText={val => this.onChangeText('Precio', val)}
-                placeholderTextColor={materialTheme.COLORS.DEFAULT}
-                style={{ borderRadius: 3, borderColor: materialTheme.COLORS.INPUT }}
-                iconContent={<Icon size={16} color={theme.COLORS.ICON} name="key" family="font-awesome" />}
-              />
+            source={{ uri: Images.Pro }}
+            style={{ height: height / 1.8, width, zIndex: 1 }}
+          >
+          <LinearGradient
+            style={styles.gradient}
+            colors={['rgba(0,0,0,0)', 'rgba(0,0,0,1)']} />
+          </ImageBackground>
+          <Block space="between" style={styles.padded}>
+            <Block>
+              <Block >
+                <Block>
+                  <Text color="white" size={60}>Unlock</Text>
+                </Block>
+                <Block>
+                  <Text color="white" size={60}>Material</Text>
+                </Block>
+                <Block row>
+                  <Text color="white" size={60}>Kit</Text>
+                  <Block middle style={styles.pro}>
+                    <Text size={16} color="white">PRO</Text>
+                  </Block>
+                </Block>
+              </Block>
+              <Text size={16} color='rgba(255,255,255,0.6)'>
+                Take advantage of all the features and screens made upon Galio Design System, coded on React Native for both.
+              </Text>
+              <Block row style={{ marginTop: theme.SIZES.BASE * 1.5, marginBottom: theme.SIZES.BASE * 4 }}>
+                <Image
+                  source={require('../assets/images/ios.png')}
+                  style={{ height: 38, width: 82, marginRight: theme.SIZES.BASE * 1.5 }} />
+                <Image
+                  source={require('../assets/images/android.png')}
+                  style={{ height: 38, width: 140 }} />
+              </Block>
               <Button
                 shadowless
                 style={styles.button}
-                color={materialTheme.COLORS.INFO}
-                onPress={() => this._signInAsync()}>
-                Iniciar Sesión
+                color={materialTheme.COLORS.BUTTON_COLOR}
+                onPress={() => navigation.navigate('Home')}>
+                GET PRO VERSION
               </Button>
             </Block>
           </Block>
         </Block>
-        </Block>
-      
+      </Block>
     );
   }
-
-  _signInAsync = async () => {
-
-      if (this.isEmpty(this.state.username) || this.isEmpty(this.state.password)){
-        Alert.alert('Datos vacios', 'Las credenciales introducidas, no son las correctas');
-        return null;
-      }else{
-        fetch('http://18.224.109.128:8000/login/', {
-          method: 'POST',
-          headers: {
-              Accept: 'application/json',
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              correo: this.state.username,
-              password: this.state.password,
-            }),
-        })
-        .then((response) => {
-
-          console.log(response);
-          return response.json();
-        })
-        .then((responseJson) => {
-          if (responseJson.access){
-            AsyncStorage.setItem('userToken', JSON.stringify(responseJson.access));
-            AsyncStorage.setItem('userTokenRefresh', JSON.stringify(responseJson.refresh));
-            this.props.navigation.navigate('Home');
-          }else{
-            Alert.alert('Error de LogIn', 'Las credenciales introducidas, no son las correctas');
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }
-  };
-
-  isEmpty(str) {
-    return (!str || 0 === str.length);
-  }
-
-  //http://18.224.109.128:8000/login/
 }
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: theme.COLORS.BLACK,
+    marginTop: Platform.OS === 'android' ? -HeaderHeight : 0,
   },
   padded: {
     paddingHorizontal: theme.SIZES.BASE * 2,
-    position: 'relative',
-    bottom: 100,
+    zIndex: 3,
+    position: 'absolute',
+    bottom: Platform.OS === 'android' ? theme.SIZES.BASE * 2 : theme.SIZES.BASE * 3,
   },
   button: {
     width: width - theme.SIZES.BASE * 4,
     height: theme.SIZES.BASE * 3,
     shadowRadius: 0,
     shadowOpacity: 0,
+  },
+  pro: {
+    backgroundColor: materialTheme.COLORS.LABEL,
+    paddingHorizontal: 8,
+    marginLeft: 12,
+    borderRadius: 2,
+    height: 22
+  },
+  gradient: {
+    zIndex: 1,
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 66,
   },
 });
